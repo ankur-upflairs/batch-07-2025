cf176de10f7e7cd7eb4c6cc4fee29e392cb75571
ankur-upflairs <ankur.upflairs@gmail.com>
2025-09-05 17:32:14 +0530

    code pushed

---
+++ java script/operators.js
@@ -0,0 +1,35 @@
+//arithmatic operators
+//+,-,*,/(divide) , % (modulus=> gives remainder) ,** (exponential)
+console.log(11%3) 
+console.log(4**3)
+//assignment operators
+// = assign to 
+let a =5;
+a+=4;//a = a+4
+a%=7;
+console.log(a)
+//comparison or relation operators => boolean 
+// < , > , <= ,>= , == ,!= (not equal to ) , === (strict check)
+console.log(5<7)
+console.log(9 >= 56)
+console.log(5=='5')
+console.log(5!=8) 
+console.log(5==='5') //check value as well as data type 
+
+//logical operators  => compare two booleans (conditions) and return boolean
+// logical and && => if both sides are true than it return true otherwise it return false
+console.log(true && false )
+console.log(true && true )
+console.log(false && true )
+console.log(false && false )
+
+//logical or || => if any side is true than return true otherwise return false
+
+console.log(true || false )
+console.log(true || true )
+console.log(false || true )
+console.log(false || false )
+//logical not !  => reverse the result 
+console.log(!true)
+
+
