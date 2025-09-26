3b7fc5285cc6208cc8aea1cf109ea302875f4cc9
ankur-upflairs <ankur.upflairs@gmail.com>
2025-09-26 16:58:56 +0530

    code pushed

---
+++ java script/if-else.js
@@ -0,0 +1,21 @@
+// let age=10
+// let age = prompt('Enter your age : ')
+// //input always string
+// age=parseInt(age)
+
+// if(age > 18){
+//     console.log('adult')
+// }
+// else {
+//     console.log('minor')
+// }
+
+// let isDay = confirm('is it a day ?')
+// if(isDay){
+//     alert('good morning')
+// }
+// else {
+//     alert('good night')
+// }
+
+
